5b42f9dc1ae8c1a81d34fdfc6f56e85f2c72de95
ryan <rajmhj510@gmail.com>
2026-09-14 16:22:53 +0000

    Fix category redirect import

--- app/admin/categories/page.tsx
+++ app/admin/categories/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import { redirect, revalidatePath } from "next/cache";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import { prisma } from "../../../lib/prisma";
 
 async function deleteCategory(formData: FormData) {
