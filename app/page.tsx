ae921bd948dd8f8beb640fc5750bd6a4506ceb99
ryan <rajmhj510@gmail.com>
2026-09-16 16:20:26 +0000

    Fix dynamic database pages

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { prisma } from "../lib/prisma";
 import HomeSearch from "./HomeSearch";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const recipes = await prisma.recipe.findMany({
     include: {
