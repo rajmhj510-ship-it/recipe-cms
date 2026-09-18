07a7c007925f07832ca34aadeec809fedd4b0bc9
ryan <rajmhj510@gmail.com>
2026-09-18 13:46:53 +0545

    Hide public footer when printing

--- app/components/PublicFooter.tsx
+++ app/components/PublicFooter.tsx
@@ -1,6 +1,6 @@
 export default function PublicFooter() {
   return (
-    <footer className="border-t border-gray-200 bg-gray-950 px-4 py-10 text-gray-400 sm:px-6 sm:py-12 lg:px-8">
+    <footer className="print:hidden border-t border-gray-200 bg-gray-950 px-4 py-10 text-gray-400 sm:px-6 sm:py-12 lg:px-8">
       <div className="mx-auto flex max-w-7xl flex-col gap-6 text-center sm:flex-row sm:items-center sm:justify-between sm:text-left">
         <div>
           <p className="text-xl font-extrabold text-white">
